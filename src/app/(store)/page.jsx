60c4a9dfe7af6d69d14edbc790305265a85ea6a0
JKukielski <jankukielski1@gmail.com>
2024-11-14 21:13:09 +0100

    testing fetch from Sanity

--- src/app/(store)/page.jsx
+++ src/app/(store)/page.jsx
@@ -1,3 +1,13 @@
+import { getAllProducts } from '@/sanity/lib/products/getAllProducts';
+import Image from 'next/image';
+
 export default async function Home() {
-  return <div>Hello World!</div>;
+  const products = await getAllProducts();
+  return (
+    <div>
+      {products.map((product) => (
+        <div key={product.id}>{product.title}</div>
+      ))}
+    </div>
+  );
 }
